bd332a8239371f06b74618d40ac8be3357ce127c
maur025 <mmoyapalli@gmail.com>
2026-05-01 20:41:04 -0400

    feat: init with introducction to Dart

---
+++ 01-dart/01_hello_world.dart
@@ -0,0 +1,10 @@
+void main(){
+  // var myName = 'Mauro';
+  // late final myName;
+  // String myName = 'Mauro';
+  const myName = 'Mauro'
+
+  print('Hola $myName');
+  print('Hola ${ myName.toUpperCase() }');
+  print('Hola ${ 1 + 1 }');
+}
---
+++ 01-dart/06_classes.dart
@@ -0,0 +1,23 @@
+void main(){
+  final wolverine = Hero(name: 'Logan', power: 'Regeneración');
+
+  print(wolverine);
+  print(wolverine.name);
+  print(wolverine.power);
+}
+
+class Hero{
+  String name;
+  String power;
+
+  // Hero(this.name, this.power);
+
+  Hero({ required this.name, this.power = 'Sin poder' });
+
+  // Hero(String pName, String pPower): name = pName, power = pPower;
+
+  @override
+  toString(){
+    return '$name - $power';
+  }
+}
